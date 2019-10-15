3d519265dda0ef217c4a8b18dfe98054bf2ba41c
Alex Pham <925workk@gmail.com>
2019-10-15 13:07:58 -0600

    added basic css for more colors

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    //change the integers below to match the height of your upper div, which I called
+    //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+    //to figure out what the scroll position is when exactly you want to fix the nav
+    //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+    //you know the position.
+    $(window).scroll(function () { 
+  
+      console.log($(window).scrollTop());
+  
+      if ($(window).scrollTop() > 100) {
+        $('#nav_bar').addClass('navbar-fixed-top');
+      }
+  
+      if ($(window).scrollTop() < 551) {
+        $('#nav_bar').removeClass('navbar-fixed-top');
+      }
+    });
+  });
