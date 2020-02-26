8e2c9d6196730f100dd7edff1095130cf230f40f
Alex Pham <925workk@gmail.com>
2020-02-26 09:48:51 -0700

    fixed profile pic.

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-$('a[href*="#"]').on('click', function(e) {
-  e.preventDefault()
+$(document).ready(function() {
+  $('a[href*="#"]').on('click', function(e) {
+    e.preventDefault()
 
-  $('html, body').animate(
-    {
-      scrollTop: $($(this).attr('href')).offset().top,
-    },
-    500,
-    'linear'
-  )
-})
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      500,
+      'linear'
+    )
+  })
 
-$(window).bind('scroll', function () {
-  if ($(window).scrollTop() > 600) {
-      $('.navbar').addClass('fixed-top');
-      $('.nav-section').fadeIn(500);
-  } else {
-      $('.navbar').removeClass('fixed-top');
-  }
-  var scrollPos = $(document).scrollTop();
-    $('#nav_bar a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('nav_bar ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-});
+  $(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 600) {
+        $('.navbar').addClass('fixed-top');
+        $('.nav-section').fadeIn(500);
+    } else {
+        $('.navbar').removeClass('fixed-top');
+    }
+    var scrollPos = $(document).scrollTop();
+      $('#nav_bar a').each(function () {
+          var currLink = $(this);
+          var refElement = $(currLink.attr("href"));
+          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+              $('nav_bar ul li a').removeClass("active");
+              currLink.addClass("active");
+          }
+          else{
+              currLink.removeClass("active");
+          }
+  });
+  });
 });
